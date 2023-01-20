4f38d8fdaf011285b4fd3e04be5e241b505617dc
Jyoti <jyoti@vns.com>
2023-01-20 14:02:32 +0530

    changed Login.js page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,35 @@
 import React from "react";
+import '../css/Login.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
 
 const Login = () => (
-    <h1>Login Page</h1>
+   <div className="container">
+    <div className="logo">
+        <img alt="" />
+    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        
+        <Form.Control type="email" placeholder="Enter email" />
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
+
 )
 
 
